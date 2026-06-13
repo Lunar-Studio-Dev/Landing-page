af66e561375951ca418a0bd00f86e73da1769661
Sagar Kemble <sagarkemble.sasa@gmail.com>
2026-06-14 03:30:32 +0530

    add tweet data

--- components/sections/community/tweets-data.ts
+++ components/sections/community/tweets-data.ts
@@ -1,146 +1,231 @@
-import type { Tweet } from "./tweet";
+import { Tweet } from "./tweet";
 
-/* Real tweets scraped from ui.aceternity.com's community wall (placeholder
-   content until Lunar Studio has its own) — avatars served from Twitter's CDN. */
 export const TWEETS: Tweet[] = [
   {
-    profile_image: "https://x.com/sagar__kemble",
-    username: "NikhilPrashar_",
-    fullname: "Nikhil Prashar",
-    blue_tick: false,
-    comment: "Excited",
-  },
-  {
-    profile_image: "https://x.com/sagar__kemble",
-    username: "dhruvsood05",
-    fullname: "Dhruv Sood",
-    blue_tick: false,
-    comment: "LESS GOOOO 🚀",
-  },
-  {
-    profile_image: "https://x.com/sagar__kemble",
-    username: "saikiran29_x",
-    fullname: "saikiran",
-    blue_tick: false,
-    comment: "looking forward. All the best 🤝",
-  },
-  {
-    profile_image: "https://x.com/sagar__kemble",
-    username: "hiteshdotcom",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1724344976715468800/MasktpKz_400x400.jpg",
+    username: "Hiteshdotcom",
     fullname: "Hitesh Choudhary",
     blue_tick: true,
-    comment: "Great start 👏",
+    comment: "Go big 😁",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2020183972102483968/cnZILaPG_400x400.jpg",
     username: "surajtwt",
     fullname: "Suraj Jha",
     blue_tick: true,
     comment: "Amazing! bss mehnat nhi rukni chhiye baaki aacha hi hoga",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1997983157581451265/-YqA55Ze_400x400.jpg",
+    username: "dhruvsood05",
+    fullname: "Dhruv Sood",
+    blue_tick: true,
+    comment: "oooo excited bro 🚀😎",
+  },
+
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1750068535018033152/KRt9ZcCk_400x400.jpg",
+    username: "Shivam_Goyal_20",
+    fullname: "Shivam Goyal",
+    blue_tick: false,
+    comment: "Make sure ingredients should be fresh and healthy 😁😁😁",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2058135330503700481/5oHW_qwe_400x400.jpg",
+    username: "Aditya989746994",
+    fullname: "Aditya Panchal",
+    blue_tick: false,
+    comment: "Let's go 🔥",
+  },
+
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2012886823018631169/RYe57bmf_400x400.jpg",
+    username: "Ashutosh10811",
+    fullname: "Ashutosh Panda",
+    blue_tick: false,
+    comment: "Crazy Guys 👏",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2012246338717753344/YvvWblXo_400x400.jpg",
+    username: "vishal_codeX",
+    fullname: "Vishal",
+    blue_tick: false,
+    comment: "🔥🔥",
+  },
+
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1997983157581451265/-YqA55Ze_400x400.jpg",
     username: "dhruvsood05",
     fullname: "Dhruv Sood",
     blue_tick: true,
     comment: "lesss gooo G 🚀",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1724344976715468800/MasktpKz_400x400.jpg",
+    username: "Hiteshdotcom",
+    fullname: "Hitesh Choudhary",
+    blue_tick: true,
+    comment: "Great start 👏",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2053376968348057600/5qxgOqP0_400x400.jpg",
+    username: "abdul_sama60108",
+    fullname: "Abdul Samad",
+    blue_tick: false,
+    comment: "Congrats brother 🙌🏻",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2047968514221514752/1qK_SsY5_400x400.jpg",
+    username: "anoop_on_x",
+    fullname: "Anoop Rajoriya",
+    blue_tick: false,
+    comment: "Ya, just do it 😁👍",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2041921708135952384/OEhzfmgE_400x400.jpg",
+    username: "MdAshraf1156",
+    fullname: "Md Ashraf Hussain",
+    blue_tick: true,
+    comment: "ooohoo jiii",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2062525702294163456/MSbBaaGo_400x400.jpg",
+    username: "codebysnorlax",
+    fullname: "Ravi Ranjan",
+    blue_tick: false,
+    comment: "🔥",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2023428202291752960/m6gzSXsH_400x400.jpg",
+    username: "NikhilPrashar_",
+    fullname: "Nikhil Prashar",
+    blue_tick: false,
+    comment: "Excited",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2052193195665285120/fxNMTVev_400x400.jpg",
+    username: "saikiran29_x",
+    fullname: "saikiran",
+    blue_tick: false,
+    comment: "looking forward. All the best 🤝",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2015299634231730176/CY61d-ij_400x400.jpg",
+    username: "hustler_ankur",
+    fullname: "Ankur",
+    blue_tick: false,
+    comment: "nice name!",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2061820042363408384/eGhGLWv-_400x400.jpg",
+    username: "AyushGhosh30804",
+    fullname: "Ayush Ghosh",
+    blue_tick: false,
+    comment: "looking great",
+  },
+  {
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2023428202291752960/m6gzSXsH_400x400.jpg",
     username: "NikhilPrashar_",
     fullname: "Nikhil Prashar",
     blue_tick: false,
     comment: "Great",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2020831974924099584/xV-jYQfK_400x400.jpg",
     username: "anandxdj",
     fullname: "Anand",
     blue_tick: false,
     comment: "🔥🔥",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1977234550708371456/lz61Pz8R_400x400.jpg",
     username: "vishesh_dev_22",
     fullname: "Vishesh Kumawat",
     blue_tick: false,
     comment: "Congratulations Sagar! You're doing really cool stuff",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1952077883486146560/jKCpzYvp_400x400.jpg",
     username: "shwetanigam_dev",
     fullname: "Shweta Nigam",
     blue_tick: false,
     comment: "Good luck! You got this!",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2058135330503700481/5oHW_qwe_400x400.jpg",
     username: "Aditya989746994",
     fullname: "Aditya Panchal",
     blue_tick: false,
     comment: "Great start 🔥",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2016352101803393027/AiN4puQu_400x400.jpg",
     username: "HappyNegi1221",
     fullname: "Happy Negi",
     blue_tick: false,
     comment: "Great work 🔥",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1919429351419834368/6htnk9Jj_400x400.jpg",
     username: "prashsainidev",
     fullname: "Prashant Saini",
     blue_tick: false,
     comment: "Nice bro keep it up !!",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1849718404569235457/FKUVD_Z3_400x400.jpg",
     username: "abhijitWebDev",
     fullname: "abhijitWebDev",
     blue_tick: true,
     comment: "Amazing !!",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/1969342593336295424/yhM78BOW_400x400.jpg",
     username: "gopalchoudharry",
     fullname: "Gopal Choudhary",
     blue_tick: false,
     comment: "Let's Go 🚀",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2049795132279672832/ipwRHpDl_400x400.jpg",
     username: "Sudhanshu7428",
     fullname: "Sudhanshu",
     blue_tick: false,
     comment: "👏👏",
   },
   {
-    profile_image: "https://x.com/sagar__kemble",
+    profile_image:
+      "https://pbs.twimg.com/profile_images/2018330735594377216/QpSMD1NH_400x400.jpg",
     username: "MehulArora121",
     fullname: "Mehul_Arora",
     blue_tick: false,
     comment: "💯",
-  },
-  {
-    profile_image: "https://x.com/sagar__kemble",
-    username: "vishal_codeX",
-    fullname: "Vishal",
-    blue_tick: false,
-    comment: "🔥🔥",
-  },
-  {
-    profile_image: "https://x.com/sagar__kemble",
-    username: "ankur_hustler_ankur",
-    fullname: "Ankur",
-    blue_tick: false,
-    comment: "nice name!",
-  },
-  {
-    profile_image: "https://x.com/sagar__kemble",
-    username: "AyushGhosh20864",
-    fullname: "Ayush Ghosh",
-    blue_tick: false,
-    comment: "looking great",
   },
 ];
